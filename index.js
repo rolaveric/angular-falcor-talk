059f4e137640d2f652e3488494cc7060b68ae45a
rolaveric <rolaveric@gmail.com>
2015-10-03 21:06:12 +1000

    Initial state

---
+++ index.js
@@ -0,0 +1,27 @@
+// From: https://github.com/Netflix/falcor-express-demo
+
+// This file starts the server and exposes the Router at /model.json
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var FalcorServer = require('falcor-express');
+var falcorRouterDemoFactory = require('falcor-router-demo');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Simple middleware to handle get/post
+app.use('/model.json', FalcorServer.dataSourceRoute(function(req, res) {
+  // Passing in the user ID, this should be retrieved via some auth system
+  return falcorRouterDemoFactory("1");
+}));
+
+app.use(express.static('public/'));
+app.use('/public/', express.static('public/'));
+
+var server = app.listen(9090, function(err) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("navigate to http://localhost:9090");
+});
